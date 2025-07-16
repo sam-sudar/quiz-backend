5a8e210217843902587c6911306b3496b813474c
iLLiSiO2 <samsudar200@gmail.com>
2025-07-16 11:00:15 +0530

    Changes to port

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const fs = require("fs");
-const PORT = 4000;
+
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
 app.get("/scores", (req, res) => {
-  fs.readFile("score.json", "utf-8", (err, data) => {
+  fs.readFile(__dirname + "/score.json", "utf-8", (err, data) => {
     if (err) {
       return res.status(500).json({ error: "Failed to read scores" });
     }
-    res.setHeader("Cache-Control", "no-cache"); // force latest data
+    res.setHeader("Cache-Control", "no-cache");
     res.json(JSON.parse(data));
   });
 });
